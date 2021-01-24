14c8058e3d2be3d4672b02433e655553b4d8cdf1
joba-1 <joachim.banzhaf@gmail.com>
2021-01-24 18:51:25 +0100

    add webserver to query measurements

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,56 @@
 #include <Arduino.h>
 
+// Web Updater
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+#include <ESP8266HTTPUpdateServer.h>
+
 #include <WiFiManager.h>
 #include <NTPClient.h>
 #include <WiFiUdp.h>
 #include <Syslog.h>
 
-#define LED_ON  LOW
+#define ARRAY_SIZE(a) (sizeof(a) / sizeof(*(a)))
+
+#define LED_ON LOW
 #define LED_OFF HIGH
 
 #define IRQ_PIN D2
 
 #define IDLE_CPM 25
 
-#define ARRAY_SIZE(a) (sizeof(a)/sizeof(*(a)))
+#define WEBSERVER_PORT 80
 
+
+ESP8266WebServer web_server(WEBSERVER_PORT);
+
+ESP8266HTTPUpdateServer esp_updater;
 
 WiFiUDP ntpUDP;
-NTPClient ntp(ntpUDP);
+NTPClient ntp(ntpUDP, NTP_SERVER);
+static char start_time[30] = "";
 
 WiFiUDP logUDP;
 Syslog syslog(logUDP, SYSLOG_PROTO_IETF);
 
+typedef struct counts {
+  uint32_t last_5s;
+  uint32_t last_1m;
+  uint32_t last_10m;
+  uint32_t last_1h;
+  uint32_t last_1d;
+} counts_t;
 
+typedef struct events
+{
+  time_t measured;
+  counts_t raw; // raw events
+  counts_t cpm; // events per minute without idle average
+} events_t;
+
+events_t events = {0};                 // last measurement
 uint32_t last_counter_reset = 0;       // millis() of last counter reset
 volatile uint32_t counter_events = 0;  // events of current interval so far
 
@@ -30,8 +59,84 @@ ICACHE_RAM_ATTR void event()
   counter_events++;
 }
 
+// Define web pages for update, reset or for event infos
+void setup_webserver()
+{
+  web_server.on("/events", HTTP_POST, []() {
+    static const char fmt[] = "{\n"
+                              " \"meta\": {\n"
+                              "  \"device\": \"" HOSTNAME "\",\n"
+                              "  \"program\": \"" PROGNAME "\",\n"
+                              "  \"version\": \"" VERSION "\",\n"
+                              "  \"cpm_idle\": %u,\n"
+                              "  \"started\": \"%s\",\n"
+                              "  \"measured\": \"%s\"\n"
+                              " },\n"
+                              " \"raw\": {\n"
+                              "  \"5s\": %u,\n"
+                              "  \"1m\": %u,\n"
+                              "  \"10m\": %u,\n"
+                              "  \"1h\": %u,\n"
+                              "  \"1d\": %u\n"
+                              " }\n"
+                              " \"cpm\": {\n"
+                              "  \"5s\": %u,\n"
+                              "  \"1m\": %u,\n"
+                              "  \"10m\": %u,\n"
+                              "  \"1h\": %u,\n"
+                              "  \"1d\": %u\n"
+                              " }\n"
+                              "}\n";
+    static char msg[sizeof(fmt) + 20 + 6 * 10];
+    static char iso_time[30];
+    time_t now = time(NULL);
+    strftime(iso_time, sizeof(iso_time), "%FT%T%Z", localtime(&now));
+    snprintf(msg, sizeof(msg), fmt, IDLE_CPM, start_time, iso_time,
+             events.raw.last_5s, events.raw.last_1m, events.raw.last_10m, events.raw.last_1h, events.raw.last_1d,
+             events.cpm.last_5s, events.cpm.last_1m, events.cpm.last_10m, events.cpm.last_1h, events.cpm.last_1d);
+      web_server.send(200, "application/json", msg);
+  });
+
+  // Call this page to reset the ESP
+  web_server.on("/reset", HTTP_POST, []() {
+    syslog.log(LOG_NOTICE, "RESET");
+    web_server.send(200, "text/plain", "Resetting");
+    delay(200);
+    ESP.restart();
+  });
+
+  // Standard page
+  static char page[] = "<html>\n"
+                       " <head><title>" PROGNAME " v" VERSION "</title></head>\n"
+                       " <body>\n"
+                       "  <h1> " PROGNAME " v" VERSION "</h1>\n"
+                       "  <table><tr>\n"
+                       "   <td><form action=\"events\" method=\"post\"><input type=\"submit\" name=\"events\" value=\"Events as JSON\" /></form></td>\n"
+                       "   <td><form action=\"reset\" method=\"post\"><input type=\"submit\" name=\"reset\" value=\"Reset\" /></form></td>\n"
+                       "  </tr></table>\n"
+                       "  Post firmware image to /update\n"
+                       " </body>\n"
+                       "</html>\n";
+
+  // Index page
+  web_server.on("/", []() {
+    web_server.send(200, "text/html", page);
+  });
+
+  // Catch all page, gives a hint on valid URLs
+  web_server.onNotFound([]() {
+    web_server.send(404, "text/html", page);
+  });
+
+  web_server.begin();
+
+  MDNS.addService("http", "tcp", WEBSERVER_PORT);
+  syslog.logf(LOG_NOTICE, "Serving HTTP on port %d", WEBSERVER_PORT);
+}
+
 
 void setup() {
+  WiFi.hostname(HOSTNAME);
   WiFi.mode(WIFI_STA);
 
   pinMode(LED_BUILTIN, OUTPUT);
@@ -65,11 +170,16 @@ void setup() {
 
   digitalWrite(LED_BUILTIN, LED_ON);
   char msg[80];
-  snprintf(msg, sizeof(msg), "Version %s, WLAN IP is %s", VERSION, WiFi.localIP().toString().c_str());
+  snprintf(msg, sizeof(msg), "%s Version %s, WLAN IP is %s", PROGNAME, VERSION, WiFi.localIP().toString().c_str());
   Serial.printf(msg);
   syslog.logf(LOG_NOTICE, msg);
 
   ntp.begin();
+
+  MDNS.begin(HOSTNAME);
+
+  esp_updater.setup(&web_server);
+  setup_webserver();
 
   last_counter_reset = millis();
   attachInterrupt(digitalPinToInterrupt(D2), event, FALLING);
@@ -80,7 +190,9 @@ bool check_ntptime() {
   static bool have_time = false;
   if( !have_time && ntp.getEpochTime() ) {
     have_time = true;
-    syslog.logf(LOG_NOTICE, "%s UTC", ntp.getFormattedTime().c_str());
+    time_t now = time(NULL);
+    strftime(start_time, sizeof(start_time), "%FT%T%Z", localtime(&now));
+    syslog.logf(LOG_NOTICE, "Booted at %s", start_time);
   }
   return have_time;
 }
@@ -101,106 +213,101 @@ void fix_cpm( uint32_t &cpm ) {
 }
 
 
-void on_interval_elapsed( uint32_t interval, uint32_t events ) {
+void on_interval_elapsed( uint32_t interval, uint32_t counts ) {
+  static bool first = true;
   static uint32_t minute_events[12];
-  static uint32_t minute_cpm = 0;
   static size_t minute_event = 0;
-  static bool minute_valid = false;
   static uint32_t ten_minute_events[10];
-  static uint32_t ten_minute_cpm = 0;
   static size_t ten_minute_event = 0;
-  static bool ten_minute_valid = false;
   static uint32_t hour_events[6];
-  static uint32_t hour_cpm = 0;
   static size_t hour_event = 0;
-  static bool hour_valid = false;
   static uint32_t day_events[24];
-  static uint32_t day_cpm = 0;
   static size_t day_event = 0;
-  static bool day_valid = false;
 
-  uint32_t cpm = (uint32_t)((uint64_t)events * 60000 / interval);
-  fix_cpm(cpm);
+  events.measured = time(NULL);
+  events.raw.last_5s = counts;
+  events.cpm.last_5s = (uint32_t)((uint64_t)counts * 60000 / interval);
+  fix_cpm(events.cpm.last_5s);
 
-  if (minute_valid == false && minute_event == 0)
-  {
+  if( first ) {
+    first = false;
     for( size_t i = 0; i < ARRAY_SIZE(minute_events); i++ ) {
-      minute_events[i] = events;
+      minute_events[i] = counts;
     }
-    minute_cpm = minute_events[0] * ARRAY_SIZE(minute_events);
-    fix_cpm(minute_cpm);
+    events.raw.last_1m = minute_events[0] * ARRAY_SIZE(minute_events);
+    events.cpm.last_1m = events.raw.last_1m;
+    fix_cpm(events.cpm.last_1m);
 
     for( size_t i = 0; i < ARRAY_SIZE(ten_minute_events); i++ ) {
       ten_minute_events[i] = minute_events[0] * ARRAY_SIZE(minute_events);
     }
-    ten_minute_cpm = ten_minute_events[0] * ARRAY_SIZE(ten_minute_events) / 10;
-    fix_cpm(ten_minute_cpm);
+    events.raw.last_10m = ten_minute_events[0] * ARRAY_SIZE(ten_minute_events);
+    events.cpm.last_10m = events.raw.last_10m / 10;
+    fix_cpm(events.cpm.last_10m);
 
     for( size_t i = 0; i < ARRAY_SIZE(hour_events); i++ ) {
       hour_events[i] = ten_minute_events[0] * ARRAY_SIZE(ten_minute_events);
     }
-    hour_cpm = hour_events[0] * ARRAY_SIZE(hour_events) / 60;
-    fix_cpm(hour_cpm);
+    events.raw.last_1h = hour_events[0] * ARRAY_SIZE(hour_events);
+    events.cpm.last_1h = events.raw.last_1h / 60;
+    fix_cpm(events.cpm.last_1h);
 
     for( size_t i = 0; i < ARRAY_SIZE(day_events); i++ ) {
       day_events[i] = hour_events[0] * ARRAY_SIZE(hour_events);
     }
-    day_cpm = day_events[0] * ARRAY_SIZE(day_events) / (24 * 60);
-    fix_cpm(day_cpm);
+    events.raw.last_1d = day_events[0] * ARRAY_SIZE(day_events);
+    events.cpm.last_1d = events.raw.last_1d / (24 * 60);
+    fix_cpm(events.cpm.last_1d);
 
     minute_event = 1;
   }
   else {
-    minute_events[minute_event] = events;
-    minute_cpm = sum(minute_events, ARRAY_SIZE(minute_events));
-    fix_cpm(minute_cpm);
+    minute_events[minute_event] = counts;
+    events.raw.last_1m = sum(minute_events, ARRAY_SIZE(minute_events));
+    events.cpm.last_1m = events.raw.last_1m;
+    fix_cpm(events.raw.last_1m);
     minute_event++;
     if( minute_event == ARRAY_SIZE(minute_events) ) {
       minute_event = 0;
-      minute_valid = true;
-
       ten_minute_events[ten_minute_event] = sum(minute_events, ARRAY_SIZE(minute_events));
-      ten_minute_cpm = (sum(ten_minute_events, ARRAY_SIZE(ten_minute_events)) + 5) / 10;
-      fix_cpm(ten_minute_cpm);
+      events.raw.last_10m = sum(ten_minute_events, ARRAY_SIZE(ten_minute_events));
+      events.cpm.last_10m = (events.raw.last_10m + 5) / 10;
+      fix_cpm(events.cpm.last_10m);
       ten_minute_event++;
       if( ten_minute_event == ARRAY_SIZE(ten_minute_events) ) {
         ten_minute_event = 0;
-        ten_minute_valid = true;
-
         hour_events[hour_event] = sum(ten_minute_events, ARRAY_SIZE(ten_minute_events));
-        hour_cpm = (sum(hour_events, ARRAY_SIZE(hour_events)) + 30) / 60;
-        fix_cpm(hour_cpm);
+        events.raw.last_1h = sum(hour_events, ARRAY_SIZE(hour_events));
+        events.cpm.last_1h = (events.raw.last_1h + 30) / 60;
+        fix_cpm(events.cpm.last_1h);
         hour_event++;
         if(hour_event == ARRAY_SIZE(hour_events) ) {
           hour_event = 0;
-          hour_valid = true;
-
           day_events[day_event] = sum(hour_events, ARRAY_SIZE(hour_events));
-          day_cpm = (sum(day_events, ARRAY_SIZE(day_events)) + 12 * 60) / (24 * 60);
-          fix_cpm(day_cpm);
+          events.raw.last_1d = sum(day_events, ARRAY_SIZE(day_events));
+          events.cpm.last_1d = (events.raw.last_1d + (12 * 60)) / (24 * 60);
+          fix_cpm(events.cpm.last_1d);
           day_event++;
           if( day_event == ARRAY_SIZE(day_events) ) {
             day_event = 0;
-            day_valid = true;
           }
         }
       }
     }
   }
 
-  syslog.logf(LOG_INFO, "%u events in %u ms => cpm curr=%u, min=%u, 10min=%u, hour=%u, day=%u",
-              events, interval, cpm, minute_cpm, ten_minute_cpm, hour_cpm, day_cpm);
+  syslog.logf(LOG_INFO, "Events CPM: 5s=%2u,  1m=%2u,  10m=%2u,  1h=%2u,  1d=%2u,  RAW: 5s=%2u,  1m=%2u,  10m=%3u,  1h=%4u,  1d=%5u",
+              events.cpm.last_5s, events.cpm.last_1m, events.cpm.last_10m, events.cpm.last_1h, events.cpm.last_1d,
+              events.raw.last_5s, events.raw.last_1m, events.raw.last_10m, events.raw.last_1h, events.raw.last_1d);
 }
 
 
-void check_events()
-{
+void check_events() {
   static const uint32_t counter_interval = 5000; // ms to accumulate irq events
 
   uint32_t now = millis();
   uint32_t elapsed = now - last_counter_reset;
-  if (elapsed >= counter_interval)
-  {
+  if( elapsed >= counter_interval ) {
     uint32_t events = counter_events;
 
     counter_events = 0;
@@ -245,5 +352,6 @@ void loop() {
     breathe();
   }
   check_events();
+  web_server.handleClient();
   delay(5);
 }
